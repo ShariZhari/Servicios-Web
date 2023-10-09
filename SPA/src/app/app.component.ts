3ec1895f932903d74b4948919e8c70941f29d65f
ShariZhari <al227832@edu.uaa.mx>
2023-10-09 07:16:19 -0600

    Section-5: Client-Registration and login

--- SPA/src/app/app.component.ts
+++ SPA/src/app/app.component.ts
@@ -10,19 +10,10 @@ import { User } from './_models/user';
 })
 export class AppComponent implements OnInit {
   title = 'SPA';
-  users:any;
 
   constructor(private http: HttpClient, private accountService: AccountService){}
   ngOnInit(): void {
-      this.getUsers();
       this.setCurrentUser();
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete:() => console.log('request completed')});
   }
   
   setCurrentUser(): void {
