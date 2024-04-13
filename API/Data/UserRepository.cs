d83d2d4d37a473f9a560f90e5e11829975fff089
ShariZhari <al227832@edu.uaa.mx>
2024-04-13 15:41:30 -0600

    Parcial 2

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -9,24 +10,46 @@ namespace API.Data;
 
 public class UserRepository : IUserRepository
 {
-    private readonly IMapper _mapper;
     private readonly DataContext _context;
-    public UserRepository(DataContext context, IMapper mapper){ //strategy pattern
-    
+    private readonly IMapper _mapper;
+
+    public UserRepository(DataContext context, IMapper mapper)
+    {
         _context = context;
         _mapper = mapper;
-
     }
 
     public async Task<MemberDto> GetMemberAsync(string username)
     {
-        return await _context.Users.Where(x => x.UserName == username).ProjectTo<MemberDto>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+        return await _context.Users
+            .Where(x => x.UserName == username)
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
     }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        return await _context.Users
-            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+        var query = _context.Users.AsQueryable();
+
+        query = query.Where(u => u.UserName != userParams.CurrentUsername);
+        query = query.Where(u => u.Gender == userParams.Gender);
+
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            "dob" => query.OrderByDescending(u => u.DateOfBirth),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
+        
+        return await PagedList<MemberDto>.CreateAsync(
+            query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider),
+            userParams.PageNumber,
+            userParams.PageSize);
     }
 
     public async Task<AppUser> GetUserByIdAsync(int id)
@@ -36,12 +59,16 @@ public class UserRepository : IUserRepository
 
     public async Task<AppUser> GetUserByUsernameAsync(string username)
     {
-        return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(u => u.UserName == username);
+        return await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(u => u.UserName == username);
     }
 
     public async Task<IEnumerable<AppUser>> GetUsersAsync()
     {
-        return await _context.Users.Include(p => p.Photos).ToListAsync();
+        return await _context.Users
+            .Include(p => p.Photos)
+            .ToListAsync();
     }
 
     public async Task<bool> SaveAllAsync()
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,19 +10,19 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(
         this IServiceCollection services,
-        IConfiguration config){
-
-        services.AddDbContext<DataContext>(opt =>{
+        IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
         services.AddCors();
-        
-        services.AddScoped<ITokenService, TokenService>(); //Scoped - Transient - Singleton - Inyeccion de dependencias
+        services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-
+        services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<LogUserActivity>();
         return services;
     }
-    
 }
